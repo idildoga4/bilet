c33ab9d84b736bc5502e65817803c9a981bc1083
idildoga4 <111755801+idildoga4@users.noreply.github.com>
2024-07-16 14:41:59 +0300

    push

---
+++ lib/pages/search_flight.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enuygun Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SearchFlight(),
+    );
+  }
+}
+
+class SearchFlight extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<SearchFlight> {
+  final TextEditingController _fromController = TextEditingController();
+  final TextEditingController _toController = TextEditingController();
+  DateTime? _selectedDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Enuygun Clone'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _fromController,
+              decoration: const InputDecoration(
+                labelText: 'Nereden?',
+                prefixIcon: Icon(Icons.flight_takeoff),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _toController,
+              decoration: const InputDecoration(
+                labelText: 'Nereye?',
+                prefixIcon: Icon(Icons.flight_land),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              leading: const Icon(Icons.date_range),
+              title: Text(
+                _selectedDate == null
+                    ? 'Tarih seç'
+                    : _selectedDate!.toLocal().toString().split(' ')[0],
+              ),
+              trailing: const Icon(Icons.keyboard_arrow_down),
+              onTap: _selectDate,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                //Uçuş arama işlemi _searchFlights()
+              },
+              child: Text('Uçuşları ara'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _selectDate() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime(2023),
+    );
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+}
